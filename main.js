ebee77599ea16c07ec41240ba7371d035830f9a5
Kenneth Dait <49531569+kpdait@users.noreply.github.com>
2021-02-10 15:51:24 -0600

    main.js: function to load main app window
    - `openMainWindow()`

--- main.js
+++ main.js
@@ -9,7 +9,25 @@ const url = require('url');
 
 let mainWindow;
 
-app.on('loaded', () -> {
-  mainWindow = new BrowserWindow({width:800,height:600});
+function openMainWindow() {
+  mainWindow = new BrowserWindow({
+    width: 1200,
+    height: 600,
+    webPreferences: {
+      contextIsolation: true
+    }
+  });
+
+  let mainUrl = url.pathToFileURL(path.join(__dirname,'mainWindow.html'));
+  mainWindow.loadURL(mainUrl.href);
+
+  // open devtools
+  mainWindow.webContents.openDevTools();
+
+  // quit app when main window closes
+  mainWindow.on('closed', () => {
+    win = null;
+    app.quit();
+  });
 }
 
