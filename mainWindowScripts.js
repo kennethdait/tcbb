97015d0e4b649e5112dfe407b57ef8cad8636abc
Kenneth Dait <49531569+kpdait@users.noreply.github.com>
2021-02-10 15:59:20 -0600

    mainWindowsScript.js: client-size javascript working

--- mainWindowScripts.js
+++ mainWindowScripts.js
@@ -1,9 +1,21 @@
 (function(){
   'use strict';
 
-  function initializePage(e) {
-    console.log(`[${new Date().toDateString()}]: page loaded successfully`);
+  function insertDateStamp() {
+    let nowStr = new Date().toString(),
+      dateTarget = document.querySelector('#date-target'),
+      txtNode = document.createTextNode(nowStr),
+      paragraph = document.createElement('p');
+    
+    paragraph.style.fontSize = '2.5em';
+    paragraph.style.fontWeight = '600';
+    return;
   }
 
-  window.on('load', initializePage);
+  window.onload = (e) => {
+    let msg = `[${new Date().toDateString()}]: page loaded successfully`;
+    console.log(msg);
+    insertDateStamp();
+  }; 
+
 }());
